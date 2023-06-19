b66217641a10c417a88af6b5226097b0fad57c3d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-06-18 23:58:04 -0300

    FEAT(item): Adicionando OnChanges()

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -7,7 +7,7 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
   @Input() dadosItem!: Item;
 
   faPen = faPen;
@@ -16,5 +16,10 @@ export class ItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void { }
+
+  ngOnChanges(): void {
+      
+  }
+
 
 }
